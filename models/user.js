98aa32f2b99efc88bd8f297ffe2f8cc5dcbe6139
Ivan Hema <hacker19088@gmail.com>
2023-06-11 22:01:42 +0300

    Update hw06

--- models/user.js
+++ models/user.js
@@ -27,6 +27,14 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationCode: {
+      type: String,
+      required: [true, 'Verify Code is required'],
+    },
   },
   { versionKey: false, timestamps: true }
 );
@@ -42,9 +50,14 @@ const loginSchema = Joi.object({
   password: Joi.string().min(6).required(),
 });
 
+const emailSchema = Joi.object({
+  email: Joi.string().required(),
+});
+
 const schemas = {
   registerSchema,
   loginSchema,
+  emailSchema,
 };
 
 const User = model('user', userSchema);
